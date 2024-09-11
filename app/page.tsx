705a27c1ec3f942cb846e7989298c4cd93be547d
Guilherme Vandanezi <guilhermevandanezi@outlook.com>
2024-09-11 09:28:02 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { ptBR } from "date-fns/locale"
+import { format } from "date-fns"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -39,12 +41,30 @@ const Home = async () => {
         },
       })
     : []
+
+  const formattedDate = format(new Date(), "EEEE, dd 'de' MMMM", {
+    locale: ptBR,
+  })
+
+  const customCapitalize = (text: string) => {
+    return text
+      .split(" ")
+      .map((word: string) =>
+        word === "de" ? word : word.charAt(0).toUpperCase() + word.slice(1),
+      )
+      .join(" ")
+  }
+
+  const capitalizedDate = customCapitalize(formattedDate)
+
   return (
     <div>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Guilherme!</h2>
-        <p>Segunda-feita, 5 de agosto</p>
+        <h2 className="text-xl font-bold">
+          {session?.user ? `Olá, ${session?.user.name}!` : "Bem vindo!"}
+        </h2>
+        <p>{capitalizedDate}</p>
         <div className="mt-6">
           <Search />
         </div>
